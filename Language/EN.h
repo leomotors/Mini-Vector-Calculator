1c08e6b2c13f83666939c9d129a64fbfdd3375bd
Leomotors <59821765+Leomotors@users.noreply.github.com>
2021-07-13 15:30:26 +0700

    EN DONE

---
+++ Language/EN.h
@@ -0,0 +1,72 @@
+#define IRASSHAI_1 "\nWelcome to Vector Calculator Program!\n\n"
+#define PRESS_ANY_KEY_TO_CONTINUE "Press any key to continue..."
+#define PROGRAM_TITLE "=====|Vector Calculator V3.3|=====\n\n"
+
+#define PLEASE_SELECT_FUNC "\nPlease select Function from below list.\n"
+#define MENU_1 "\nPlease select Function from below list.\n"
+#define MENU_2 "[2] Do operations!\n"
+#define MENU_3 "[3] Import & Export Vectors\n"
+#define MENU_4 "[4] Delete all Vectors\n"
+#define MENU_5 "[5] Settings\n"
+
+#define MENU_EXIT "[0] Exit\n"
+
+#define YOUR_CHOICE "Your Choice: "
+#define EXIT_THANK_YOU "Thanks for using this program! Press any key to exit..."
+#define INVALID_CHOICE "Invalid choice, please try again.\n"
+
+#define ONE_VEC_OP "\tOne Vector Operation\n"
+#define VEC_OP_1 "[1] Find size of a Vector\n"
+#define VEC_OP_2 "[2] Multiply Vector with scalar\n"
+#define TWO_VEC_OP "\tTwo Vectors Operation\n"
+#define VEC_OP_3 "[3] Add 2 Vectors\n"
+#define VEC_OP_4 "[4] Dot Product of 2 Vectors\n"
+#define VEC_OP_5 "[5] Cross Product of 2 Vectors\n"
+#define VEC_OP_6 "[6] Project a vector on another vector\n"
+#define VEC_OP_7 "[7] Find area of parrallelogram from 2 vectors\n"
+
+#define SELECTED_OP "Selected Operation: "
+#define SELECT_VECTOR "Select Vector: "
+#define VECTOR_NOT_AVAILABLE "This vector not available, Press any key to continue..."
+#define SEL_FIRST_VEC "Select First Vector: "
+#define SEL_SECOND_VEC "Select Second Vector: "
+#define ONE_OR_BOTH_NOT_AVAILABLE "One or Both of vector not available, Press any key to continue..."
+#define SIZE_OF_VEC_IS "Size of Vector #%d is "
+#define ENTER_SCALAR "Enter scalar to multiply with: "
+#define DOT_PROD_IS "Dot Product is "
+#define AREA_IS "Area is "
+#define SQ_UNIT " Sq.Unit\n"
+#define OP_DONE_AND_PRESS_ANY_KEY "Operation done, Please any to continue..."
+
+#define SETTINGS_TITLE "\n=====|Settings|=====\n\n"
+#define SETTINGS_1 "[1] Set terminal (command prompt)'s color\n"
+#define SETTINGS_2 "[2] Set Number Precision\n"
+#define BACK_0 "[0] Back\n"
+
+#define N_DIGITS_A_DECIMAL "Number of digits after decimal: "
+#define N_DIGITS_TRACEBACK "Number of digits must be between 0 and 6!\n"
+
+#define FILE_MENU "\n=====|File Menu|=====\n\n"
+#define FILE_1 "[1] Import Vector\n"
+#define FILE_2 "[2] Export Vector\n"
+
+#define ONLYWINDOWS "This is only supported on Windows!\n"
+
+#define WHICH_SLOT "Which slot you want? : "
+#define VECTOR_ALR_EXIST "Vector already exists, Overwrite? [Y/N]: "
+#define ENTER_VECTOR "Please enter vector (i,j,k): "
+#define INVALID_INDEX_PAKTC "Invalid index!\nPress any key to continue!"
+
+#define VECTOR_N "Vector #%d : %s\n"
+
+#define RESULT_VECTOR_IS "Result Vector is %s\n"
+#define DO_YOU_WANT_TO_SAVE "Do you want to save vector? [Y/N]: "
+#define WHERE_TO_SAVE "Where you want to save vector? : "
+#define INDEX_OUT_OF_RANGE "Can't save here, Index out of range\n"
+#define ALREADY_EXIST "This slot already has vector in it. Overwrite? [Y/N]: "
+#define THIS_WILL_DELETE_ALL_VEC "Warning: This action will delete all vector. Continue? [Y/N]: "
+#define ALL_VEC_DELETED "All vectors have been deleted, press any to continue..."
+
+#define ENTER_FNAME "Enter file name: "
+#define ERROR_OPENING_FILE "Error upon opening files, File may not exist.\n"
+#define F_ALREADY_EXIST "File already exists, Overwrite? [Y/N]: "
